92567657e3be0e615a83aaa86df23ffe05422e41
Tom-Boudeville <119631325+Tom-Boudeville@users.noreply.github.com>
2026-05-26 18:38:26 +0200

    add all composant to projet

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,18 @@
 import Header from "./components/Header";
 import BackGroundImageDesign from "./components/BackGroundImageDesign";
 import PresentationPart from "./components/PresentationPart.jsx";
-import ProjectSeparator from "./components/ProjectSeparator.jsx";
+import ProjectPresentationPart from "./components/ProjectPresentationPart.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <>
+    <div style={{ backgroundColor: "#F9FAFF", position: "relative" }}>
       <Header />
       <BackGroundImageDesign />
       <PresentationPart />
-      <ProjectSeparator />
-    </>
+      <ProjectPresentationPart />
+      <Footer />
+    </div>
   );
 }
 
