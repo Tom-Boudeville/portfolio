8a5f146d91dd30d58c0d38b56b808ce61b9725e1
Tom-Boudeville <119631325+Tom-Boudeville@users.noreply.github.com>
2026-05-26 20:31:54 +0200

    cahnge: presentationcard to reuse it

--- portfolio/src/components/PresentationCard.jsx
+++ portfolio/src/components/PresentationCard.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 
-export default function PresentationCard() {
+export default function PresentationCard({
+  subtitle,
+  title,
+  description,
+  button1,
+  button2,
+}) {
   return (
-    <section className="relative z-20 flex items-center w-full px-8 md:px-16 min-h-[600px]">
+    <section className="relative z-20 flex items-center w-full px-8 md:px-16 min-h-[500px]">
       <div className="max-w-lg mt-12">
         <p className="text-primary font-bold font-poppins text-2xl mb-4 uppercase">
-          UI/UX Designer
+          {subtitle}
         </p>
         <p className="text-6xl md:text-7xl font-black font-poppins text-gray-900 mb-6">
-          Hello, my name is Madelyn Torff
+          {title}
         </p>
         <p className="text-gray-400 font-poppins text-2xl mb-8 text-justify">
-          Short text with details about you, what you do or your professional
-          career. You can add more information on the about page.
+          {description}
         </p>
         <div className="flex space-x-4">
           <button className="bg-primary text-[20px] font-bold font-medium py-2 px-8 rounded-lg">
-            Projects
+            {button1}
           </button>
-          <button className="font-semibold border-2 text-[20px] border-dark py-2 px-8 rounded-lg ">
-            Linkedin
-          </button>
+          {button2 && (
+            <button className="font-semibold border-2 text-[20px] border-dark py-2 px-8 rounded-lg ">
+              {button2}
+            </button>
+          )}
         </div>
       </div>
     </section>
--- portfolio/src/components/PresentationPart.jsx
+++ portfolio/src/components/PresentationPart.jsx
@@ -6,7 +6,13 @@ export default function PresentationPart() {
     <section className="relative flex  bg-transparent items-center px-8 md:px-16 overflow-hidden">
       <div className="relative w-full flex flex-col md:flex-row z-10">
         <div className="w-full md:w-1/2">
-          <PresentationCard />
+          <PresentationCard
+            subtitle="UI/UX Designer"
+            title="Hello,  my name is Madelyn Torff"
+            description="Short text with details about you, what you do or your professional career. You can add more information on the about page."
+            button1="Projects"
+            button2="Linkedin"
+          />
         </div>
       </div>
     </section>
