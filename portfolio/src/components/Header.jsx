ce3656fe8cb86d4874534a3fc389ab480a4a6a26
Tom-Boudeville <119631325+Tom-Boudeville@users.noreply.github.com>
2026-05-26 19:01:33 +0200

    change: correction supperposition image and add link route

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="z-50 flex w-full h-20 overflow-hidden">
+    <header className="relative z-50 flex w-full h-20 overflow-hidden">
       <div className="flex-1 flex items-center px-8 md:px-16">
         <span className="text-2xl font-bold">Madelyn Torff</span>
       </div>
 
-      <nav className=" flex-1 flex items-center justify-end px-8 md:px-16 space-x-8">
-        <a href="#about">About</a>
+      <nav className="flex-1 flex items-center justify-end px-8 md:px-16 space-x-8">
+        <Link to="/about">About</Link>
         <a href="#projects">Projects</a>
         <a href="#contacts">Contacts</a>
       </nav>
