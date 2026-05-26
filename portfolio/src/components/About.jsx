f18364b95b3cd2f4b856dbad58cb0832c7aadc5b
Tom-Boudeville <119631325+Tom-Boudeville@users.noreply.github.com>
2026-05-26 21:04:07 +0200

    add: about page complete change: delete useless bold on header

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 export default function About() {
-  return <></>;
+  return (
+    <main className="flex flex-col md:flex-row items-center justify-between px-8 md:px-16 mt-24 max-w-6xl mx-auto gap-12">
+      <div className="flex-1">
+        <h1 className="text-5xl font-bold text-gray-900 mb-6 font-playfair">
+          About me
+        </h1>
+        <p className="text-gray-500 mb-8 leading-relaxed text-xl font-nunito">
+          Nisl arcu, scelerisque neque ut. Tincidunt amet, tempor duis tortor
+          neque auctor dis ipsum. Pretium cras amet odio amet eleifend id sed
+          cras sed. Aliquet risus posuere aliquet imperdiet sit.
+        </p>
+        <button className="bg-primary text-gray-900 px-6 py-2 rounded-lg font-poppins font-medium shadow-md hover:shadow-lg transition-shadow">
+          Resume
+        </button>
+      </div>
+
+      <div className="flex-1 flex justify-center">
+        <div className="relative w-80 h-80 md:w-96 md:h-96 lg:w-[450px] lg:h-[450px] rounded-full bg-primary overflow-hidden flex items-end justify-center shadow-lg">
+          <img
+            src="/bg-about.png"
+            alt="Madelyn Torff"
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="relative z-50 flex w-full h-20 overflow-hidden">
       <div className="flex-1 flex items-center px-8 md:px-16">
-        <span className="text-2xl font-bold">Madelyn Torff</span>
+        <span className="text-2xl ">Madelyn Torff</span>
       </div>
 
       <nav className="flex-1 flex items-center justify-end px-8 md:px-32 space-x-16 text-lg">
-        <Link to="/about">About</Link>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `transition-all hover:text-cyan-400 ${
+              isActive ? "border-b-2 border-gray-900" : ""
+            }`
+          }
+        >
+          About
+        </NavLink>
         <a href="/#projects">Projects</a>
         <a href="/#contacts">Contacts</a>
       </nav>
