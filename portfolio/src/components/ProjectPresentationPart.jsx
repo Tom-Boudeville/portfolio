4e323f98abf8318d874e9eb8144f6ce12fa3631b
Tom-Boudeville <119631325+Tom-Boudeville@users.noreply.github.com>
2026-05-26 19:29:10 +0200

    fix: project presentation part alignement and size

--- portfolio/src/components/ProjectPresentationPart.jsx
+++ portfolio/src/components/ProjectPresentationPart.jsx
@@ -4,30 +4,30 @@ import Project from "./Project";
 
 export default function ProjectPresentationPart() {
   return (
-    <div
-      className="flex flex-col items-center space-y-12 max-w-4xl mx-auto px-4"
-      style={{ width: "67%", margin: "0 auto" }}
+    <section
+      id="projects"
+      className="w-full px-8 md:px-16 mt-40 flex flex-col space-y-12 max-w-6xl mx-auto"
     >
       <ProjectSeparator />
       <Project
         url="/project1.png"
-        title="Project 1"
+        title="Project Name"
         description="I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."
         imageFirst={false}
       />
 
       <Project
         url="/project2.png"
-        title="Project 2"
+        title="Project Name"
         description="What was your role, your deliverables, if the project was personal, freelancing."
       />
 
       <Project
         url="/project3.png"
-        title="Project 3"
+        title="Project Name"
         description="You can also add in this description the type of the project, if it was for web, mobile, electron."
         imageFirst={false}
       />
-    </div>
+    </section>
   );
 }
