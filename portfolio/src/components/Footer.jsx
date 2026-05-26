95a47d43c2ab0a57c9c241e489ef62e181d5a3ba
Tom-Boudeville <119631325+Tom-Boudeville@users.noreply.github.com>
2026-05-26 18:38:46 +0200

    add : footer

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center h-20 mt-16">
+      <div>
+        <div className="flex flex-row gap-4 mb-2 justify-center">
+          <img src="/instagram.png" alt="Logo" className="w-6 h-6 mb-1" />
+          <img src="/linkedin.png" alt="Logo" className="w-6 h-6 mb-1" />
+          <img src="/mail.png" alt="Logo" className="w-6 h-6 mb-1" />
+        </div>
+        <span className="text-sm text-gray-600">Madelyn Torff 2021</span>
+      </div>
+    </footer>
+  );
+}
